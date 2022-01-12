f7c2ae668af227623b73745ede175f6dc77edb68
HeyoItsGabo <96260919+HeyoItsGabo@users.noreply.github.com>
2022-01-12 12:33:43 -0600

    Create Lost in Space.py

---
+++ Lost in Space.py
@@ -0,0 +1,33 @@
+import time
+
+
+def title():
+    line1 = r"""  _              _        _            ____                    """
+
+    line2 = r""" | |    ___  ___| |_     (_)_ __      / ___| _ __   __ _  ___ ___ """
+
+    line3 = r""" | |   / _ \/ __| __|    | | '_ \     \___ \| '_ \ / _` |/ __/ _ \ """
+
+    line4 = r""" | |__| (_) \__ \ |_     | | | | |     ___) | |_) | (_| | (_|  __/ """
+
+    line5 = r""" |_____\___/|___/\__|    |_|_| |_|    |____/| .__/ \__,_|\___\___| """
+
+    line6 = r"""                                            |_| """
+
+    creds = "Developed by Gabe Wightman"
+
+    time.sleep(0.4)
+    print(line1)
+    time.sleep(0.4)
+    print(line2)
+    time.sleep(0.4)
+    print(line3)
+    time.sleep(0.4)
+    print(line4)
+    time.sleep(0.4)
+    print(line5)
+    time.sleep(0.4)
+    print(line6)
+    time.sleep(2)
+    print(creds)
+    time.sleep(2)
