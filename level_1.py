bb63d9be4a9688618d0a2671423eab3cb41f1aeb
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-01-25 13:50:24 -0600

    Update level_1.py

--- level_1.py
+++ level_1.py
@@ -1,24 +1,31 @@
 import time
 
 
-def firstlevel():
+def partone():
     print("You are on Starship Epsilon-7, headed for the planet Europa. "
           "\nYou are peacefully asleep, in your bunk. You are awoken a shrieking"
           "\nmetallic sound, as the starship slows down considerably.")
     time.sleep(0.5)
     print("1- Go back to sleep")
     print("2- Get out of bed")
-    try:
-        getup = input()
 
-        if int(getup) == 1:
-            print("yeet")
+    getup = input()
 
-        elif int(getup) == 2:
-            print("yoink")
+    if int(getup) == 1:
+        print("Choosing to ignore the sound, you go back to sleep.")
 
-        else:
-            print("1 or 2")
 
-    except:
-        print("1 or 2!!!")
+    elif int(getup) == 2:
+        print("yoink")
+
+
+    else:
+        print("1 or 2")
+
+
+def parttwo():
+    pass
+
+
+def partthree():
+    pass
