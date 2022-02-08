955f8f49733d7da9e164791b6755c2db25ba622f
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-02-08 14:46:39 -0600

    Update level_1.py

--- level_1.py
+++ level_1.py
@@ -12,20 +12,30 @@ def partone():
     time.sleep(0.5)
     print("1- Go back to sleep")
     print("2- Get out of bed")
+    try:
+        getup = input()
 
-    getup = input()
+        if int(getup) == 1:
+            print("Choosing to ignore the sound, you go back to sleep. The spaceship plummets to "
+                  "\nthe planet below, taking you with it.")
 
-    if int(getup) == 1:
-        print("Choosing to ignore the sound, you go back to sleep. You die.")
-        partone()  # Restarts the level if you choose the wrong path
+            while True:
+                restart = input("You have died. Input the enter key to restart. ")
 
-    elif int(getup) == 2:
-        print("As your feet touch the metal floor, you notice that it is "
-              "\nmuch hotter than usual. ")
-        parttwo()  # Starts the next part
+                if restart == "":
+                    partone()  # Restarts the level if you choose the wrong path
+                    return False
+                else:
+                    print("Please hit the enter key to continue. ")
 
-    else:
-        print("1 or 2")
+        elif int(getup) == 2:
+            print("As your feet touch the metal floor, you notice that it is "
+                  "\nmuch hotter than usual. ")
+            parttwo()  # Starts the next part
+
+    except ValueError:
+        print("Please enter 1 or 2.")
+        partone()
 
 
 def parttwo():
