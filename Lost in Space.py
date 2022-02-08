8e5481b96d3991fa5edc9801b7e23e30ce0b1c69
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-02-08 13:26:45 -0600

    Update Lost in Space.py

--- Lost in Space.py
+++ Lost in Space.py
@@ -1,17 +1,16 @@
 import time
-from functions import title, startgame
+from functions import title
+from level_1 import startgame
 
 
-accept = False
-
-if not accept:
+def terms():
     title()
     time.sleep(1)
     terms = input("By hitting enter, you are agreeing not to use elements from this game"
                   "\nwithout permission from the developer. ")
     if terms == "":
-        accept = True
         startgame()
 
     else:
         exit()
+    
