78d3cb5b8ae8c5daae05586b6a167f3fd34b0e54
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-02-08 13:57:39 -0600

    Update Lost in Space.py

--- Lost in Space.py
+++ Lost in Space.py
@@ -1,16 +1,20 @@
 import time
 from functions import title
-from level_1 import startgame
+from level_1 import level1
+from level_2 import level2
 
 
 def terms():
-    title()
+    title()  # Starts the title sequence
     time.sleep(1)
-    terms = input("By hitting enter, you are agreeing not to use elements from this game"
-                  "\nwithout permission from the developer. ")
-    if terms == "":
-        startgame()
+    accept = input("By hitting enter, you are agreeing not to use elements from this game"
+                   "\nwithout permission from the developer. ")
+    if accept == "":
+        level1()  # Starts level 1
+        level2()  # Starts level 2
 
     else:
         exit()
-    
+
+
+terms()
