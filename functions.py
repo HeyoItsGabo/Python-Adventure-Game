18ff206db2a2f03106dd5efa8db678d4c8110095
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-02-08 13:27:15 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 import time
-from level_1 import level1
 
 def title():
     line1 = r"""  _              _        _            ____                    """
@@ -30,7 +29,3 @@ def title():
     print(line6)
     time.sleep(1)
     print(creds)
-
-
-def startgame():
-    level1()
