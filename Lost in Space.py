352db81be2441f39fb01ab9e2dd94de5034893f7
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-02-09 13:38:52 -0600

    Update Lost in Space.py

--- Lost in Space.py
+++ Lost in Space.py
@@ -2,6 +2,9 @@ import time
 from functions import title
 from level_1 import level1
 from level_2 import level2
+from level_3 import level3
+from termcolor import cprint
+
 
 
 def terms():
@@ -10,8 +13,12 @@ def terms():
     accept = input("By hitting enter, you are agreeing not to use elements from this game"
                    "\nwithout permission from the developer. ")
     if accept == "":
+        time.sleep(1)
+        cprint("NOTE: You will need an internet connection for some puzzles!", 'red', attrs=['underline'])
+        time.sleep(1)
         level1()  # Starts level 1
-        level2()  # Starts level 2
+        level2()  # Starts level 2 once level 1 is completed
+        level3()  # Starts level 3 once both level 1 and 2 are completed
 
     else:
         exit()
