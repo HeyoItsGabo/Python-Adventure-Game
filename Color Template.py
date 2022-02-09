c83537c832390804dee30a06935795ea91b04114
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-02-09 13:59:46 -0600

    Create Color Template.py

---
+++ Color Template.py
@@ -0,0 +1,10 @@
+from termcolor import cprint
+
+cprint("YEETUS", 'grey')
+cprint("YEETUS", 'red')
+cprint("YEETUS", 'green')
+cprint("YEETUS", 'yellow')
+cprint("YEETUS", 'blue')
+cprint("YEETUS", 'magenta')
+cprint("YEETUS", 'cyan')
+cprint("YEETUS", 'white')
