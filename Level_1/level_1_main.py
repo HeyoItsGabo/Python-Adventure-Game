45081f7c86dfc06e4c935cb701c32637e712f54a
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-04-05 14:33:05 -0500

    Update level_1_main.py

--- Level_1/level_1_main.py
+++ Level_1/level_1_main.py
@@ -1,9 +1,24 @@
 import time
+import random
 from tqdm import tqdm
 from termcolor import cprint, colored
+from functions import menu, Window 
 
 HasBackPack = False
 HasHealthPack = False
+NoShoes = False
+FlipFlops = False
+WorkBoots = False
+codesegment1 = random.randint(0, 9)
+codesegment2 = random.randint(0, 9)
+codesegment3 = random.randint(0, 9)
+code = int(str(codesegment1) + str(codesegment2) + str(codesegment3))
+items = ["Window", ]
+code1location = random.randint(1, 3)
+code2location = random.randint(4, 6)
+code3location = random.randint(7, 9)
+
+
 
 
 def level1():
@@ -21,8 +36,8 @@ def level1():
     time.sleep(1)
     cprint("What do you do?", 'green')
     time.sleep(1)
-    cprint("1- Go back to sleep", 'green')
-    cprint("2- Get out of bed", 'green')
+    cprint("1- Go back to sleep"
+           "\n2- Get out of bed", 'green')
 
     while True:  # First encounter in the first level
         wakeup = input()
@@ -42,7 +57,6 @@ def level1():
                         time.sleep(1)
 
             elif int(wakeup) == 2:
-                print("yoink")
                 break
             else:
                 cprint("Please enter either 1 or 2.", 'green')
@@ -55,20 +69,35 @@ def level1():
     time.sleep(1)
     cprint("Which do you choose?", 'green')
     time.sleep(1)
+    cprint("1- Neither pair of shoes"
+           "\n2- The pair of flip flops"
+           "\n3- The pair of work boots")
 
     while True:  # Second encounter in the first level
         footwear = input()
         try:
             if int(footwear) == 1:
-                pass
+                cprint("Choosing not to take any pair of shoes, you head towards the door.")
+                global NoShoes
+                NoShoes = True
+                break
             elif int(footwear) == 2:
-                pass
+                cprint("You slip the flip flops on, but still feel the warmth from the floor.")
+                global FlipFlops
+                FlipFlops = True
+                break
             elif int(footwear) == 3:
-                pass
+                cprint("You put on the boots, and tie the laces. ")
+                global WorkBoots
+                WorkBoots = True
+                break
             else:
-                pass
+                cprint("Please enter a value 1-3.")
         except ValueError:
             cprint("Please enter either 1, 2, or 3.", 'green')
+            
+    while True:
+        pass
 
 
 level1()
