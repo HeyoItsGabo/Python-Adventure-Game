e9bc8453ee5b21659fd6b47064088a870eff586f
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-04-05 14:32:33 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import time
+from termcolor import colored, cprint
+
 
 def title():
     line1 = r"""  _              _        _            ____                    """
@@ -16,16 +18,87 @@ def title():
     creds = "Developed by Gabe Wightman"
 
     time.sleep(0.4)
-    print(line1)
+    cprint(line1, 'green')
     time.sleep(0.4)
-    print(line2)
+    cprint(line2, 'green')
     time.sleep(0.4)
-    print(line3)
+    cprint(line3, 'green')
     time.sleep(0.4)
-    print(line4)
+    cprint(line4, 'green')
     time.sleep(0.4)
-    print(line5)
+    cprint(line5, 'green')
     time.sleep(0.4)
-    print(line6)
+    cprint(line6, 'green')
     time.sleep(1)
-    print(creds)
+    cprint(creds, 'green')
+
+
+def menu(list, question):
+    for item in list:
+        print(list.index(item), item)
+    while True:
+        result = input(question)
+        try:
+            result = int(result)
+            break
+        except ValueError:
+            cprint("Selection must be a whole number 0-9.", 'green')
+    return result
+
+
+def door(code):
+    cprint("You walk to the bulkhead, and see a keypad. Based on what you see,"
+           "\nthe code must be 4 digits long. ")
+    while True:
+        try:
+            option1 = int(input(colored("Digit one: ", 'green')))
+            break
+        except ValueError:
+            cprint("Digit one must be a whole number 0-9.", 'green')
+    while True:
+        try:
+            option2 = int(input(colored("Digit two: ", 'green')))
+            break
+        except ValueError:
+            cprint("Digit two must be a whole number 0-9.", 'green')
+    while True:
+        try:
+            option3 = int(input(colored("Digit three: ", 'green')))
+            break
+        except ValueError:
+            cprint("Digit three must be a whole number 0-9.", 'green')
+
+            
+    chosencode = int(str(option1) + str(option2) + str(option3))
+    print("")
+    if chosencode == code:
+        cprint("The keypad flashes green, as the bulkhead opens.", 'green')
+        return 1
+    else:
+        cprint("The keypad beeps once, and flashes red. The "
+               "\ninputted code is wrong", 'green')
+        return 0
+
+
+def Window(choice, codelocation, codevalue):
+    print("")
+    cprint("You look at the window. Only four inches of reinforced glass"
+           "\nbetween you and the void.", 'green')
+    if choice == codelocation:
+        cprint("Carved into the frame, you see the number", 'green' + str(codevalue) + ".", 'green')
+        print("")
+    else:
+        cprint("You find no code.", 'green')
+        print("")
+    
+    
+def Backpack(choice, codelocation, codevalue):
+    print("")
+    cprint("Rifling through the backpack, you find a scratched up notebook.", 'green')
+    if choice == codelocation:
+        cprint("Scribbled into one of the pages, you see the number", 'green' + str(codevalue) + ".", 'green')
+        print("")
+    else:
+        cprint("You find no code.", 'green')
+        print("")
+        
