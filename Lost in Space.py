8b1054d04ff26c833c7dd25a64d8d049bb1f497f
Gabriel W <96260919+HeyoItsGabo@users.noreply.github.com>
2022-04-06 14:23:05 -0500

    Update Lost in Space.py

--- Lost in Space.py
+++ Lost in Space.py
@@ -10,21 +10,21 @@ def terms():
     title()  # Starts the title sequence
     time.sleep(1)
 
-    accept = input(colored("By hitting enter, you are agreeing not to use elements from this game"
-                           "\nwithout permission from the developer. ", 'green'))
+    accept = input("By hitting enter, you are agreeing not to use elements from this game"
+                   "\nwithout permission from the developer. ")
     if accept == "":
         time.sleep(1)
         print("")  # Formatting
-        cprint("NOTE: You will need an internet connection for some puzzles!", 'red', attrs=['underline'])
+        cprint("NOTE: You will need an internet connection for some puzzles!", attrs=['underline'])
         print("")  # Formatting
         time.sleep(2)
 
         cprint("To play this game, you must choose from a list of options presented "
                "\nto you. Insert the number of the choice you want to make. Keep in "
-               "\nmind that your decisions will affect the later portions of the game!", 'green', attrs=['underline'])
+               "\nmind that your decisions will affect the later portions of the game!", attrs=['underline'])
         print("")
         while True:  # Conditional for accepting the rules of the game
-            accept_rules = input(colored("Hit the enter key if you understand the rules of this game. ", 'green'))
+            accept_rules = input("Hit the enter key if you understand the rules of this game. ")
             if accept_rules == "":
                 level1()  # Starts level 1
                 level2()  # Starts level 2 once level 1 is completed
